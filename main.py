3845f03df1448d510f981d7b1485d212e95efa56
Konosuke Sakai <konosukeam@gmail.com>
2025-06-24 13:14:25 +0900

    feat(main.py): implement Streamlit interface for XPS dashboard with buttons for thermal management and battery configuration options

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
+import subprocess
+
+import streamlit as st
+
+
 def main():
-    print("Hello from xps-dashboard!")
+    st.title("XPS dashboard")
+
+    st.header("ThermalManagement")
+
+    quiet_clicked = st.button("Quiet")
+    ultra_performance_clicked = st.button("UltraPerformance")
+
+    st.header("PrimaryBattChargeCfg")
+
+    adaptive_clicked = st.button("Adaptive")
+    prim_ac_use_clicked = st.button("PrimAcUse")
+
+    cctk = "C:\\Program Files (x86)\\Dell\\Command Configure\\X86_64\\cctk.exe"
+
+    if quiet_clicked:
+        sudo([cctk, "--ThermalManagement=Quiet"])
+
+    if ultra_performance_clicked:
+        sudo([cctk, "--ThermalManagement=UltraPerformance"])
+
+    if adaptive_clicked:
+        sudo([cctk, "--PrimaryBattChargeCfg=Adaptive"])
+
+    if prim_ac_use_clicked:
+        sudo([cctk, "--PrimaryBattChargeCfg=PrimAcUse"])
+
+
+def sudo(args: list[str]) -> None:
+    subprocess.run(["sudo.exe", *args])
 
 
 if __name__ == "__main__":
